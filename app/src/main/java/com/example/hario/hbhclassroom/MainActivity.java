bf81484d9cf6bbd23e17079380716ad7821e0147
hario <hariomgupta73@gmail.com>
2018-04-03 21:42:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hario/hbhclassroom/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.hario.hbhclassroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+private Button studentLogin;
+private Button facultyLogin;
+private Button notice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        studentLogin = findViewById(R.id.Main_student);
+        notice = findViewById(R.id.Main_notice);
+        facultyLogin = findViewById(R.id.Main_faculty);
+
+
+        // Student ONclick
+        studentLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+        //Notice Onclick
+notice.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+
+    }
+});
+
+//faculty onclick
+facultyLogin.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+
+    }
+});
+
+
+
+
+
+
+
+    }
+}
