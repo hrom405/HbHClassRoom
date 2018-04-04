7140a686b045d9268a5ffef96a8ad96ac75f1246
Aditya7121 <adityab.4869@gmail.com>
2018-04-04 16:27:18 +0530

    fgdf

--- app/src/main/java/com/example/hario/hbhclassroom/MainActivity.java
+++ app/src/main/java/com/example/hario/hbhclassroom/MainActivity.java
@@ -20,7 +20,7 @@ private Button notice;
         facultyLogin = findViewById(R.id.Main_faculty);
 
 // Aditya Ka Chan
-        // Student ONclick
+        // Student ONcdrgdrglick
         studentLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
